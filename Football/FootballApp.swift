29fc19e1d89420875e55d2e7451a114e8886bf2f
Aaron Muuo <aaronmuuo60@gmail.com>
2026-01-30 17:23:11 +0300

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Aaron Muuo on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
